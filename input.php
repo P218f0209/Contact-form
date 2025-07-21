e8b29de93dafb4b9e930841d25842fe16d88b7c0
yugo suzuki <218f0209@s.obirin.ac.jp>
2025-07-21 12:57:56 +0900

    Update input.php

--- input.php
+++ input.php
@@ -4,17 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MAIL FORM</title>
+  <link rel="stylesheet" href="contact.css">
 </head>
 <body>
   <form method="POST" action="index.php">
     名前:<input type="text" name="name" required value="<?php makeBackform('name'); ?>"><br>
     e-mail:<input type="email" name="email" required value="<?php makeBackform('email'); ?>"><br>
     件名:<input type="text" name="title" required value="<?php makeBackform('title'); ?>"><br>
-    <textarea name="msg" required><?php makeBackform('msg'); ?></textarea>
-    <br>
+    <textarea name="msg" required><?php makeBackform('msg'); ?></textarea><br>
+    <label>
+      <input type="checkbox" name="agree" value="yes" required>
+      プライバシーポリシーに同意します
+    </label><br>
+    
     
     <input type="submit" name="confirm" value="確認画面へ">
 
   </form>
 </body>
 </html>
+
